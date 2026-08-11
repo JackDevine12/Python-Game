1697b54db76bd7f5d3eec6340f5ba5119c871cab
JackDevine12 <jackdevine202@gmail.com>
2026-08-11 12:23:12 +0100

    Initial Setup of pygame and gameDisplay Objects

---
+++ Game.py
@@ -0,0 +1,8 @@
+#Import pygame module
+import pygame
+#Intailise pygame object
+pygame.init()
+#Define game display object, 800px wide and 600px tall
+gameDisplay = pygame.display.set_mode((800,600))
+#Define game caption/title of game window
+pygame.display.set_caption('A bit Racey')
