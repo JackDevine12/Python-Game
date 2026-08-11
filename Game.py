18886e0f001ba2ce254f8a6658b9e4220910d87f
JackDevine12 <jackdevine202@gmail.com>
2026-08-11 21:06:53 +0100

    Added function to display text for when player crashes

--- Game.py
+++ Game.py
@@ -32,6 +32,25 @@ def text_objects(text, font):
     textSurface = font.render(text, True, black)
     #Return the text surface object and its rectangular area
     return textSurface, textSurface.get_rect()
+def message_display(text):
+    #Create a font object using the specified font and size
+    largeText = pygame.font.Font('freesansbold.ttf',115)
+    #Call the text_objects function to create a text surface object and its rectangular area
+    TextSurf, TextRect = text_objects(text, largeText)
+    #Set the center of the rectangular area to the center of the game display
+    TextRect.center = ((display_width/2),(display_height/2))
+    #Draw the text surface object on the game display at the specified rectangular area
+    gameDisplay.blit(TextSurf, TextRect)
+    #Update the game display to show the changes made
+    pygame.display.update()
+    #Pause the game for 2 seconds to allow the player to read the message
+    time.sleep(2)
+    #Call the game_loop function to restart the game
+    game_loop()
+
+def crash():
+    #Call the message_display function to show the "You Crashed" message on the game display
+    message_display('You Crashed')
 def game_loop():
 #Define the initial x and y coordinates for the car image
     x = (display_width * 0.45)
@@ -48,7 +67,8 @@ def game_loop():
             #Check if the event is a quit event, if so set crashed to True to exit the while loop
             if event.type == pygame.QUIT:
                 #Update gameExit variable to True to exit the while loop
-                gameExit = True
+                pygame.quit()
+                quit()
             if event.type == pygame.KEYDOWN:
                 #Check if the left arrow key is pressed, if so set x_change to -5 to move the car left
                 if event.key == pygame.K_LEFT:
@@ -67,10 +87,8 @@ def game_loop():
         car(x,y)
         #Check if the car has gone off the screen, if so set crashed to True to exit the while loop
         if x > display_width - car_width or x < 0:
-            #If the car goes off the screen, set crashed to True to exit the while loop
-            gameExit = True
-        #Print event variable value
-        print(event)
+            #If the car goes off the screen, call the crash function to display the crash message
+            crash()
         #Fill the game display with a color, in this case white
         pygame.display.update()
         #Set the clock to tick at 60 frames per second
